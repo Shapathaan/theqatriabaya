47f490a4327b1edef8311551764cfb519e9cbf36
Shapathaan <shapathaan@gmail.com>
2026-01-20 21:18:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-/* NAVIGATION */
 function navigate(id){
   document.querySelectorAll("section").forEach(s=>s.classList.remove("active"));
   document.getElementById(id).classList.add("active");
 }
 
-/* LOAD PRODUCTS */
 function loadProducts(){
   db.collection("products").onSnapshot(snapshot=>{
     const list = document.getElementById("productList");
@@ -15,9 +13,11 @@ function loadProducts(){
         <div class="card"
           onmouseenter="this.querySelector('video')?.play()"
           onmouseleave="this.querySelector('video')?.pause()">
+          
           <div class="media">
             <video src="${p.video}" muted loop></video>
           </div>
+
           <div class="card-content">
             <h4>${p.name}</h4>
             <p>₹${p.price}</p>

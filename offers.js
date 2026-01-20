028ef93c2e027625cd4e544bc6fb12d54467e8b5
Shapathaan <shapathaan@gmail.com>
2026-01-20 20:04:02 +0530

    Update offers.js

--- offers.js
+++ offers.js
@@ -1,14 +1,1 @@
-const OFFERS = [
-  {
-    title: "Ramzan Special",
-    banner: "assets/offers/ramzan.jpg",
-    discount: "Flat 20% OFF",
-    expiry: "2026-04-10"
-  },
-  {
-    title: "Eid Offer",
-    banner: "assets/offers/eid.jpg",
-    discount: "Buy 2 Get 1",
-    expiry: "2026-04-15"
-  }
-];
+

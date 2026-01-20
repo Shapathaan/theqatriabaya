867bb36225729a6160cbdf98875168054937ef4f
Shapathaan <shapathaan@gmail.com>
2026-01-20 18:51:54 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,21 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "Embroidered Black Abaya",
+    price: 1999,
+    badge: "BESTSELLER",
+    image: "assets/products/abaya1.jpg",
+    video: "assets/products/abaya1.mp4",
+    sizes: ["S","M","L","XL","XXL"]
+  },
+  {
+    id: 2,
+    name: "Classic Open Abaya",
+    price: 1499,
+    badge: "NEW",
+    image: "assets/products/abaya2.jpg",
+    video: "",
+    sizes: ["M","L","XL"]
+  }
+];
 
